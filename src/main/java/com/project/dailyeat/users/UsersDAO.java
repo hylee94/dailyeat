22ce8859725313680ee2c84e6ee4eb73714976cd
hylee94 <gmlsendol@gmail.com>
2024-08-12 14:27:42 +0900

    회원가입/로그인

---
+++ src/main/java/com/project/dailyeat/users/UsersDAO.java
@@ -0,0 +1,19 @@
+package com.project.dailyeat.users;
+
+
+import com.project.dailyeat.common.DBConnPool;
+
+public class UsersDAO extends DBConnPool {
+    public UsersDAO() {
+        super();
+    }
+
+    //DB 연동코드, Connection Pool 사용
+
+
+    //사용자가 입력한 데이터 DB에 추가
+    public void UsersInsert(UsersDTO dto) {
+
+    }
+
+}
